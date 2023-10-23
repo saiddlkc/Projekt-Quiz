d3d41a0ed5884422cbc4f037afd9e7ae4ee7bc23
saiddlkc <saidalkilic@gmail.com>
2023-10-23 15:50:52 +0200

    eine quiz frage wird gestellt und beendet

--- index.js
+++ index.js
@@ -1,21 +1,48 @@
 import readlineSync from "readline-sync";
-// import chalk from "chalk";
-// import chalkAnimation from "chalk-animation";
-// import { createSpinner } from "nanospinner";
+import chalk from "chalk";
+import { createSpinner } from "nanospinner";
 import { setTimeout as sleep } from "timers/promises";
+import figlet from "figlet";
 
-var userName = readlineSync.question("Wer bist du denn? ");
-
-// var figlet = require("figlet");
-
-// figlet(`Willkommen ${userName}`, function (err, data) {
-//   console.log(data);
-// });
+let userName = readlineSync.question(chalk.yellow("Wer bist du denn? "));
 console.clear();
-console.log("Sag das doch gleich " + userName + " Willkommen bei unserem Quiz");
+figlet(
+  `Willkommen beim JS  QUIZ. | MASTER   ${userName}`,
+  function (err, data) {
+    console.log(data);
+  }
+);
+await sleep(1000);
 
-await sleep(3000); // Zeitverzögerung in ms
+let ask = readlineSync.question(
+  chalk.yellow("Wollen wir Anfangen? (y) / (n) ")
+);
+if (ask.toLowerCase() === "y") {
+  console.clear();
+  console.log(
+    "Welches der folgenden wörter wird verwendet, um eine Variable zu definieren?"
+  );
+  await sleep(1000);
+  console.log(chalk.yellow("1. const"));
+  await sleep(1000);
+  console.log(chalk.yellow("2. let"));
+  await sleep(1000);
+  console.log(chalk.yellow("3. 1 und 2"));
+  await sleep(1000);
+  console.log(chalk.yellow("4. shabang"));
+  let correctAnswer = 3;
 
-// console.log("Wähle aus in welcher Kategorie du dein Quiz spielen willst");
+  let userAnswer = readlineSync.question("(Gib die Nummer der Antwort ein): ");
 
-let score = 0;
+  if (parseInt(userAnswer) === correctAnswer) {
+    console.log(chalk.green("Richtige Antwort!"));
+  } else {
+    console.log(
+      chalk.red("Falsche Antwort. Die richtige Antwort ist 3 (1 und 2).")
+    );
+  }
+} else if (ask.toLowerCase() === "n") {
+  console.log("Spiel abgebrochen.");
+} else {
+  console.log("Ungültige Eingabe. Bitte nur 'y' oder 'n' eingeben.");
+}
