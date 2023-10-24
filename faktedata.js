0bfc472dfee34344cd8d5ecd11491b7a591c90c2
saiddlkc <saidalkilic@gmail.com>
2023-10-24 15:50:29 +0200

    alpha done

---
+++ faktedata.js
@@ -0,0 +1,69 @@
+const questions = [
+  {
+    question: "Welches Land ist das größte der Welt?",
+    options: ["a) Russland", "b) China", "c) Indien", "d) USA"],
+    answer: "a",
+  },
+  {
+    question: "Was ist die Hauptstadt von Frankreich?",
+    options: ["a) Berlin", "b) Madrid", "c) Paris", "d) London"],
+    answer: "c",
+  },
+  {
+    question: "Wer hat die Relativitätstheorie entwickelt?",
+    options: [
+      "a) Isaac Newton",
+      "b) Albert Einstein",
+      "c) Galileo Galilei",
+      "d) Stephen Hawking",
+    ],
+    answer: "b",
+  },
+  {
+    question: "Welcher Planet ist der vierte in unserem Sonnensystem?",
+    options: ["a) Mars", "b) Venus", "c) Jupiter", "d) Saturn"],
+    answer: "a",
+  },
+  {
+    question: "Wie viele Kontinente gibt es auf der Erde?",
+    options: ["a) 5", "b) 6", "c) 7", "d) 8"],
+    answer: "c",
+  },
+  {
+    question: "Welcher Fluss fließt durch Kairo?",
+    options: ["a) Nil", "b) Rhein", "c) Donau", "d) Amazonas"],
+    answer: "a",
+  },
+  {
+    question: "Welcher Ozean liegt zwischen den USA und Europa?",
+    options: [
+      "a) Pazifischer Ozean",
+      "b) Atlantischer Ozean",
+      "c) Indischer Ozean",
+      "d) Arktischer Ozean",
+    ],
+    answer: "b",
+  },
+  {
+    question: "Was ist die Hauptstadt von Japan?",
+    options: ["a) Peking", "b) Tokio", "c) Seoul", "d) Bangkok"],
+    answer: "b",
+  },
+  {
+    question: "Welches chemische Element hat das Symbol 'Fe'?",
+    options: ["a) Sauerstoff", "b) Eisen", "c) Silber", "d) Gold"],
+    answer: "b",
+  },
+  {
+    question: "Welches Buch ist der erste Band der Harry-Potter-Reihe?",
+    options: [
+      "a) Harry Potter und der Gefangene von Askaban",
+      "b) Harry Potter und der Orden des Phönix",
+      "c) Harry Potter und der Stein der Weisen",
+      "d) Harry Potter und der Halbblutprinz",
+    ],
+    answer: "c",
+  },
+];
+
+export default questions;
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import readlineSync from "readline-sync";
 import chalk from "chalk";
-import { createSpinner } from "nanospinner";
 import { setTimeout as sleep } from "timers/promises";
 import figlet from "figlet";
+import importedQuestions from "./faktedata.js";
+console.log(importedQuestions[0].question);
+console.log(importedQuestions[0].options);
 
 console.clear;
 let userName = readlineSync.question(chalk.yellow("Name eingeben:"));
@@ -12,10 +14,23 @@ figlet(`Willkommen bei meinem QUIZ.  ${userName}`, function (err, data) {
 });
 await sleep(1000);
 console.log(chalk.yellowBright("LOS GEHTS "));
-await sleep(3000);
+await sleep(2000);
+let wrongAnswerCount = 0;
+let lastChance =
+  " Jetzt musst du aufpassen, noch einen Fehler dürfen wir uns nicht erlauben.";
+
+function endGame(wrongAnswerCount) {
+  if (wrongAnswerCount > 1) {
+    figlet("GAME OVER", function (err, data) {
+      console.log(chalk.red(data));
+      process.exit();
+    });
+  }
+}
 
 // FRAGE 1
 console.clear();
+console.log(chalk.blue("FRAGE 1"));
 console.log("Wie viele Kontinente gibt es auf der Erde?");
 console.log(chalk.yellow("a) 5"));
 console.log(chalk.yellow("b) 6"));
@@ -30,18 +45,22 @@ let userAnswer = readlineSync.question(
 if (userAnswer.toLowerCase() === correctAnswer) {
   console.log(
     chalk.green(
-      `Richtige Antwort, gut gemacht ${userName}! Hier kommt die nächste Frage`
+      `Richtige Antwort, gut gemacht ${userName}! Hier kommt die nächste Frage `
     )
   );
 } else {
   console.log(
-    chalk.red("Falsche Antwort. Die richtige Antwort ist 'c' (7 Kontinente).")
+    chalk.red(
+      `Falsche Antwort. Die richtige Antwort ist ${correctAnswer}.${lastChance}`
+    )
   );
+  wrongAnswerCount++;
+  await sleep(3000);
 }
-await sleep(2000);
 
 // FRAGE 2
 console.clear();
+console.log(chalk.blue("FRAGE 2"));
 console.log("Wie lautet die Hauptstadt von Spanien?");
 console.log(chalk.yellow("a) Madrid"));
 console.log(chalk.yellow("b) Barcelona"));
@@ -59,13 +78,19 @@ if (userAnswer2.toLowerCase() === correctAnswer2) {
   );
 } else {
   console.log(
-    chalk.red("Falsche Antwort. Die richtige Antwort ist 'c' (Madrid).")
+    chalk.red(
+      `Falsche Antwort. Die richtige Antwort ist ${correctAnswer2}.${lastChance}`
+    )
   );
+  wrongAnswerCount++;
 }
-// FRAGE 3
+
+endGame(wrongAnswerCount);
 await sleep(2000);
 
+// FRAGE 3
 console.clear();
+console.log(chalk.blue("FRAGE 3"));
 console.log("Welcher Berg ist der höchste Berg der Welt?");
 console.log(chalk.yellow("a) Zugspitze"));
 console.log(chalk.yellow("b) Schöneberg"));
@@ -83,13 +108,18 @@ if (userAnswer3.toLowerCase() === correctAnswer3) {
   );
 } else {
   console.log(
-    chalk.red("Falsche Antwort. Die richtige Antwort ist 'c' (Mount Everest).")
+    chalk.red(
+      `Falsche Antwort. Die richtige Antwort ist ${correctAnswer3}.${lastChance}`
+    )
   );
+  wrongAnswerCount++;
 }
+endGame(wrongAnswerCount);
 
 await sleep(2000);
 // FRAGE 4
 console.clear();
+console.log(chalk.blue("FRAGE 4"));
 console.log("In welcher Stadt lebte der Detektiv Sherlock Holmes?");
 console.log(chalk.yellow("a) Düsseldorf"));
 console.log(chalk.yellow("b) Manchester"));
@@ -109,13 +139,18 @@ if (userAnswer4.toLowerCase() === correctAnswer4) {
   );
 } else {
   console.log(
-    chalk.red("Falsche Antwort. Die richtige Antwort ist 'c' (London).")
+    chalk.red(
+      `Falsche Antwort. Die richtige Antwort ist ${correctAnswer4}.${lastChance}`
+    )
   );
+  wrongAnswerCount++;
 }
+endGame(wrongAnswerCount);
 
 await sleep(2000);
 // FRAGE 5
 console.clear();
+console.log(chalk.blue("FRAGE 5"));
 console.log("Welches Unternehmen hat das iPhone entwickelt?");
 console.log(chalk.yellow("a) Microsoft"));
 console.log(chalk.yellow("b) Apple"));
@@ -131,19 +166,24 @@ if (userAnswer5.toLowerCase() === correctAnswer5) {
   console.log(chalk.green(`du bist Krass ${userName}! Weiter gehts`));
 } else {
   console.log(
-    chalk.red("Falsche Antwort. Die richtige Antwort ist 'b' Apple.")
+    chalk.red(
+      `Falsche Antwort. Die richtige Antwort ist ${correctAnswer5}.${lastChance}`
+    )
   );
+  wrongAnswerCount++;
 }
+endGame(wrongAnswerCount);
 await sleep(2000);
 // FRAGE 6
 
 console.clear();
+console.log(chalk.blue("FRAGE 6"));
 console.log("In welchem Land haben die Spartaner gelebt?");
 console.log(chalk.yellow("a) Griechenland"));
 console.log(chalk.yellow("b) Türkei"));
 console.log(chalk.yellow("c) Ägypten"));
 console.log(chalk.yellow("d) Azeroth"));
-let correctAnswer6 = "c";
+let correctAnswer6 = "a";
 
 let userAnswer6 = readlineSync.question(
   "(Gib die Antwort ein - a, b, c oder d): "
@@ -153,14 +193,18 @@ if (userAnswer6.toLowerCase() === correctAnswer6) {
   console.log(chalk.green(`Super ${userName}! Weiter so`));
 } else {
   console.log(
-    chalk.red("Falsche Antwort. Die richtige Antwort ist 'a' Griechenland.")
+    chalk.red(
+      `Falsche Antwort. Die richtige Antwort ist ${correctAnswer6}.${lastChance}`
+    )
   );
+  wrongAnswerCount++;
 }
-
+endGame(wrongAnswerCount);
 await sleep(2000);
 // FRAGE 7
 
 console.clear();
+console.log(chalk.blue("FRAGE 7"));
 console.log("Welcher Schauspieler spielte die Hauptrolle in Inception");
 console.log(chalk.yellow("a) Johnny Depp"));
 console.log(chalk.yellow("b) Booker De Witt"));
@@ -176,13 +220,18 @@ if (userAnswer7.toLowerCase() === correctAnswer7) {
   console.log(chalk.green(`Super ${userName}! Weiter so`));
 } else {
   console.log(
-    chalk.red("Falsche Antwort. Die richtige Antwort ist d Leonardo DiCaprio.")
+    chalk.red(
+      `Falsche Antwort. Die richtige Antwort ist ${correctAnswer7}.${lastChance}`
+    )
   );
+  wrongAnswerCount++;
 }
+endGame(wrongAnswerCount);
 await sleep(2000);
 // FRAGE 8
 
 console.clear();
+console.log(chalk.blue("FRAGE 8"));
 console.log(
   "Welches Videospiel ermöglicht es Spielern, in einer offenen Welt Bauklötze zu platzieren und zu entfernen?"
 );
@@ -200,13 +249,18 @@ if (userAnswer8.toLowerCase() === correctAnswer8) {
   console.log(chalk.green(`du bist echt Gut ${userName}! fast geschafft`));
 } else {
   console.log(
-    chalk.red("Falsche Antwort. Die richtige Antwort ist a Minecraft.")
+    chalk.red(
+      `Falsche Antwort. Die richtige Antwort ist ${correctAnswer8}.${lastChance}`
+    )
   );
+  wrongAnswerCount++;
 }
-
+endGame(wrongAnswerCount);
+await sleep(2000);
 // FRAGE 9
 
 console.clear();
+console.log(chalk.blue("FRAGE 9"));
 console.log(
   "Welches Videospiel basiert auf einem postapokalyptischen Ödland und bietet zahlreiche Möglichkeiten für Entscheidungen und Handlungen?"
 );
@@ -224,13 +278,18 @@ if (userAnswer9.toLowerCase() === correctAnswer9) {
   console.log(chalk.green(`du bist echt Gut ${userName}! fast geschafft`));
 } else {
   console.log(
-    chalk.red("Falsche Antwort. Die richtige Antwort ist d Fallout.")
+    chalk.red(
+      `Falsche Antwort. Die richtige Antwort ist ${correctAnswer9}.${lastChance}`
+    )
   );
+  wrongAnswerCount++;
 }
-
+endGame(wrongAnswerCount);
+await sleep(2000);
 // FRAGE 10
 
 console.clear();
+console.log(chalk.blue("LETZTE KNOCKOUT FRAGE"));
 console.log(" Welches Gas atmen Pflanzen ein und welches Gas geben sie ab");
 console.log(chalk.yellow("a) Sauerstoff ein, Kohlendioxid ab "));
 console.log(chalk.yellow("b) Kohlendioxid ein, Sauerstoff ab"));
@@ -243,16 +302,11 @@ let userAnswer10 = readlineSync.question(
 );
 
 if (userAnswer10.toLowerCase() === correctAnswer10) {
-  figlet(
-    `DU HAST ES GESCHAFFT  ${userName} DU DARFST JETZT WEITER UND DIE WELT ERKUNDEN`,
-    function (err, data) {
-      console.log(data);
-    }
-  );
+  figlet(`DU HAST GEWONNEN ${userName}`, function (err, data) {
+    console.log(chalk.green(data));
+  });
 } else {
-  console.log(
-    chalk.red(
-      "Falsche Antwort. Die richtige Antwort ist b Kohlendioxid ein, Sauerstoff ab."
-    )
-  );
+  endGame();
+  wrongAnswerCount++;
 }
+endGame(wrongAnswerCount);
