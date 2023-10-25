0aeb2d9a796b3507146e2d3f47070071e7fd6632
saiddlkc <saidalkilic@gmail.com>
2023-10-25 19:40:36 +0200

    Projekt done

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import readlineSync from "readline-sync";
 import chalk from "chalk";
 import { setTimeout as sleep } from "timers/promises";
 import figlet from "figlet";
+import enquirer from "enquirer";
 import importedQuestions from "./faktedata.js";
 
 console.clear;
@@ -11,25 +12,37 @@ figlet(`Willkommen bei meinem QUIZ.  ${userName}`, function (err, data) {
   console.log(data);
 });
 await sleep(1000);
-console.log(chalk.yellowBright("LOS GEHTS "));
-await sleep(2000);
+console.log(
+  chalk.yellowBright(
+    "Hilf Dobby zu entkommen, indem du 6 Fragen richtig beantwortest. Aber pass auf, wenn du zu viele Fehler machst, muss Dobby sterben."
+  )
+);
+
+await enquirer.prompt({
+  type: "input",
+  name: "continue",
+  message: "Drücke Enter, um fortzufahren...",
+});
 let score = 0; // zähl richtige antwort
 let countAnswer = 0; // zähl wieviele Fragen richtig gemacht gib ende zurück
 let wrongAnswerCount = 0; // zähl wieviele Fragen falsch gemacht
 let lastChance =
-  " Jetzt musst du aufpassen, noch einen Fehler dürfen wir uns nicht erlauben.";
+  "  Jetzt musst du aufpassen, noch einen Fehler und Dobbys kopf wird rollen.";
 
 //  Zähl  wv Fragen richtig wenn größer als 5 WIN
 function winGame(score) {
-  if (score > 5)
+  if (score > 5) {
     figlet("GEWONNEN", function (err, data) {
       console.clear();
       console.log(
-        chalk.blue(`du hast  ${countAnswer} Fragen richtig beantwortet`)
+        chalk.blue(
+          `${userName} hat ${countAnswer} Fragen richtig beantwortet. Dobby ist jetzt FREI!`
+        )
       );
       console.log(chalk.green(data));
       process.exit();
     });
+  }
 }
 
 //  Zähl wv Fragen falsch wenn größer als 1 dann beende Spiel
@@ -38,7 +51,9 @@ function endGame(wrongAnswerCount) {
     console.clear();
     figlet("GAME OVER", function (err, data) {
       console.log(
-        chalk.blue(`du hast  ${countAnswer} Fragen richtig beantwortet`)
+        chalk.blue(
+          `${userName} hat nur ${countAnswer} Fragen richtig beantwortet und zu viele Fehler gemacht. Dobby muss jetzt sterben.`
+        )
       );
       console.log(chalk.red(data));
       process.exit();
@@ -65,11 +80,7 @@ while (currentQuestion) {
   );
 
   if (userAnswer.toLowerCase() === currentQuestion.answer) {
-    console.log(
-      chalk.green(
-        `Richtige Antwort, gut gemacht ${userName}! Hier kommt die nächste Frage `
-      )
-    );
+    console.log(chalk.green(`Richtige Antwort, ${userName}! Weiter... `));
     score++;
     countAnswer++;
     winGame(score);
